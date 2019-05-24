a93037782edad21fe1fd63b1bcd921058993d6b5
nate423 <nate.423@gmail.com>
2019-05-23 22:26:06 -0700

    Add files via upload

---
+++ sensor.js
@@ -0,0 +1,31 @@
+var five = require("johnny-five"),
+  board, potentiometer;
+
+board = new five.Board();
+
+board.on("ready", function() {
+
+  // Create a new `potentiometer` hardware instance.
+  potentiometer = new five.Sensor({
+    pin: "A0",
+    freq: 250
+  });
+
+  // Inject the `sensor` hardware into
+  // the Repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    pot: potentiometer
+  });
+
+  // "data" get the current reading from the potentiometer
+  potentiometer.on("change", function() {
+    console.log(this.value);
+  });
+});
+
+
+// References
+//
+// http://arduino.cc/en/Tutorial/AnalogInput
+
