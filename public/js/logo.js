21988e439f6ac15195775e57c384c41d35461f8f
wei170 <guocheng@guochengwei.com>
2019-05-26 02:41:00 -0700

    Setup a server * The server communicates with the sensor with node-dweetio * The server communicates with the website with socket.io

--- public/js/logo.js
+++ public/js/logo.js
@@ -1,5 +1,7 @@
-var WIDTH = 400*4;
-var HEIGHT = 230*4;
+const socket = io();
+
+var WIDTH = 400 * 4;
+var HEIGHT = 230 * 4;
 var NUM_POINTS = 2000;
 
 var points = [];
@@ -9,7 +11,7 @@ var t = 0;
 var jitter = 0;
 var minJitter = 5;
 
-var lastMousePosition = {x: 0, y: 0};
+var lastMousePosition = { x: 0, y: 0 };
 var lastTime = new Date();
 
 var delaunay;
@@ -24,17 +26,22 @@ function generatePoints(image, numPoints) {
   scratchCanvas.height = image.height;
   scratchContext.drawImage(image, 0, 0, WIDTH, HEIGHT);
 
-  var imageData = scratchContext.getImageData(0, 0, scratchCanvas.width, scratchCanvas.height);
+  var imageData = scratchContext.getImageData(
+    0,
+    0,
+    scratchCanvas.width,
+    scratchCanvas.height
+  );
 
   var points = [];
   for (var i = 0; i < numPoints; i++) {
     var x = Math.round(Math.random() * (imageData.width - 1));
     var y = Math.round(Math.random() * (imageData.height - 1));
-    var color = imageData.data[y * (imageData.width * 4) + (x * 4)];
+    var color = imageData.data[y * (imageData.width * 4) + x * 4];
     while (color == 0) {
       x = Math.round(Math.random() * (imageData.width - 1));
       y = Math.round(Math.random() * (imageData.height - 1));
-      color = imageData.data[y * (imageData.width * 4) + (x * 4)];
+      color = imageData.data[y * (imageData.width * 4) + x * 4];
     }
 
     var p = [x, y];
@@ -57,9 +64,7 @@ function drawPoints(points, context) {
       drawLine(p2, p3, context);
       // drawLine(p3, p1, context);
     }
-  } catch (e) {
-
-  }
+  } catch (e) {}
 }
 //triangle size and line width
 function drawLine(p1, p2, context) {
@@ -70,25 +75,26 @@ function drawLine(p1, p2, context) {
   if (dist > 50 && dist < 100) {
     context.lineWidth = 0.05;
     //dist *= 10;
-      context.strokeStyle = lineColor(dist / 100);
-  } else if(dist < 30 && dist > 20){
-      context.lineWidth = .05;
+    context.strokeStyle = lineColor(dist / 100);
+  } else if (dist < 30 && dist > 20) {
+    context.lineWidth = 0.05;
     //dist *= 10;
-      context.strokeStyle = lineColor(dist / 100);
-  } else if(dist < 20 && dist > 10){
-      context.lineWidth = 0.15;
-     // dist *= 10;
-      context.strokeStyle = lineColor(dist / 100);
-  } else if(dist < 10 && dist > 3){
-      context.lineWidth = 0.25;
-     // dist *= 10;
-      context.strokeStyle = lineColor(dist / 100);
-  } else if(dist < 3) { //dot size
-      context.lineWidth = 5;
-      //dist *= 10;
-      context.strokeStyle = dotColor(dist / 100);
+    context.strokeStyle = lineColor(dist / 100);
+  } else if (dist < 20 && dist > 10) {
+    context.lineWidth = 0.15;
+    // dist *= 10;
+    context.strokeStyle = lineColor(dist / 100);
+  } else if (dist < 10 && dist > 3) {
+    context.lineWidth = 0.25;
+    // dist *= 10;
+    context.strokeStyle = lineColor(dist / 100);
+  } else if (dist < 3) {
+    //dot size
+    context.lineWidth = 5;
+    //dist *= 10;
+    context.strokeStyle = dotColor(dist / 100);
   } else {
-      context.lineWidth = 0.001;
+    context.lineWidth = 0.001;
   }
   //context.strokeStyle = gray(dist / 100);
 
@@ -101,7 +107,7 @@ function drawLine(p1, p2, context) {
 //line color
 function lineColor(value) {
   value = Math.min(Math.round(value), 255);
- return 'rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
+  return 'rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
 }
 // dot color
 function dotColor(value) {
@@ -118,9 +124,14 @@ function draw() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 
   for (var i = 0; i < points.length; i++) {
-    points[i][0] = points[i].original[0] + noise.perlin3(points[i].original[0], points[i].original[1], t / 2) * (jitter + minJitter);
+    points[i][0] =
+      points[i].original[0] +
+      noise.perlin3(points[i].original[0], points[i].original[1], t / 2) *
+        (jitter + minJitter);
     points[i][1] =
-        (points[i].original[1] + noise.perlin3(points[i].original[0], points[i].original[1], t / 2 + 100) * (jitter + minJitter));
+      points[i].original[1] +
+      noise.perlin3(points[i].original[0], points[i].original[1], t / 2 + 100) *
+        (jitter + minJitter);
   }
 
   drawPoints(points, context);
@@ -144,7 +155,7 @@ var logoImage = new Image();
 
 logoImage.onload = function() {
   window.onmousemove = mouseMove;
-  canvas = document.getElementById("logocanvas");
+  canvas = document.getElementById('logocanvas');
   context = canvas.getContext('2d');
 
   canvas.width = WIDTH;
@@ -152,9 +163,13 @@ logoImage.onload = function() {
 
   points = generatePoints(logoImage, NUM_POINTS);
   delaunay = new Delaunator(points);
-  
+
   requestAnimationFrame(draw);
-    
 };
 
 logoImage.src = 'profile.png';
+
+socket.on('value', (value) => {
+  console.log(value);
+  jitter += value;
+});
